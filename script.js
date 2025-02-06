3914405666df6bb724130136f49a3933b6baa7ab
Sakshamsetia <setiasaksham15@gmail.com>
2025-02-07 00:57:23 +0530

    Completed the File Inputter except Image Display

---
+++ script.js
@@ -0,0 +1,24 @@
+function one(){
+    document.getElementById("i1").style.display = "block";
+    document.getElementById("i2").style.display = "none";
+    document.getElementById("i3").style.display = "none";
+    document.getElementById("i4").style.display = "none";
+}
+function two(){
+    document.getElementById("i1").style.display = "block";
+    document.getElementById("i2").style.display = "block";
+    document.getElementById("i3").style.display = "none";
+    document.getElementById("i4").style.display = "none";
+}
+function three(){
+    document.getElementById("i1").style.display = "block";
+    document.getElementById("i2").style.display = "block";
+    document.getElementById("i3").style.display = "block";
+    document.getElementById("i4").style.display = "none";
+}
+function four(){
+    document.getElementById("i1").style.display = "block";
+    document.getElementById("i2").style.display = "block";
+    document.getElementById("i3").style.display = "block";
+    document.getElementById("i4").style.display = "block";
+}
